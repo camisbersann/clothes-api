dea6d1d0dfd782fe4c8811cd05df25be6cfe015d
camilabersan <camilabersan.564@gmail.com>
2023-11-17 08:18:58 -0300

    feat: server.js

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import  express from "express";
+import { config } from "dotenv";
+import rotas from "./routes/index.routes.js";
+
+config();
+
+const port = process.env.PORT || 5002;
+
+const app = express();
+app.use(express.json());
+app.use(rotas);
+
+app.listen(port, () =>
+    console.log(`Server started on http://localhost:${port}`)
+);
+
